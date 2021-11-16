d4f872b0c493edeb776135b10d71fe9ea2d09932
Valoušek Petr <Valousek.Petr@stud.vassboskovice.cz>
2021-11-16 10:14:00 +0100

    Přidejte soubory projektu.

---
+++ hadancisel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hadancisel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
